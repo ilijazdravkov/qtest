b7ab3634b7271d00c87b848b9c4b22189cad8489
ILIJA ZDRAVKOV <ilija.zdravkov@polipanelgroup.com>
2020-04-13 19:49:42 +0200

    header label modifications

--- app/views/master.view.php
+++ app/views/master.view.php
@@ -7,14 +7,41 @@
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
+
+<?php
+$headerLink;
+$headerLabel;
+switch (\Core\Httpd\Request::uri()){
+    case 'register':
+        $headerLink = '<li class="nav-item"><a href="/login" class="nav-link icon d-flex align-items-center"><i class="ion-ios-people mr-2"></i> Login</a></li>';
+        $headerLabel = "REGISTER";
+        break;
+    case 'login':
+        $headerLink = '<li class="nav-item"><a href="/register" class="nav-link icon d-flex align-items-center"><i class="ion-ios-people mr-2"></i> Register</a></li>';
+        $headerLabel = "LOGIN";
+        break;
+    default:
+        break;
+}
+?>
 <div class="main-section">
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="">LOGIN</a>
+            <a class="navbar-brand" href="">
+                <?php
+                if(isset($headerLabel)) {
+                    echo $headerLabel;
+                }
+                ?>
+            </a>
 
             <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a href="/register" class="nav-link icon d-flex align-items-center"><i class="ion-ios-people mr-2"></i> Register</a></li>
+                    <?php
+                    if(isset($headerLink)){
+                        echo $headerLink;
+                    }
+                    ?>
                 </ul>
             </div>
         </div>
