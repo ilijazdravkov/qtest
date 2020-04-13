d950bbb25c6e3ef1772b02fac18933cde3ad74f3
ILIJA ZDRAVKOV <ilija.zdravkov@polipanelgroup.com>
2020-04-14 01:54:40 +0200

    strong password implemented

--- app/Support/Validation/UserValidator.php
+++ app/Support/Validation/UserValidator.php
@@ -20,6 +20,19 @@ class UserValidator implements UserValidatorContract
 
         $validation->validate();
 
+        if(!$validation->fails()){
+            $uppercase = preg_match('@[A-Z]@', $data['password']);
+            $lowercase = preg_match('@[a-z]@', $data['password']);
+            $number = preg_match('@[0-9]@', $data['password']);
+            $specialChars = preg_match('@[^\w]@', $data['password']);
+
+            if(!$uppercase || !$lowercase || !$number || !$specialChars) {
+                $strongPassErrorMsg = 'Password must have at least one upper case letter, one number, and one special character.';
+                //$validation->setMessage('password', $strongPassErrorMsg);
+                $validation->errors->add('password', '', $strongPassErrorMsg);
+            }
+        }
+
         return  $validation;
     }
 
