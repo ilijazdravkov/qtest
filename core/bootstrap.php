e64b02d6e04a92c3fe8b108df484ecb4dd0edcc8
ILIJA ZDRAVKOV <ilija.zdravkov@polipanelgroup.com>
2020-04-13 14:25:41 +0200

    add view helper function and master view

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -10,3 +10,14 @@ DB::setConnection(Connection::make(require 'config/database.php')); // establish
 $router = Router::load('routes/routes.php'); // load routes file
 
 $router->direct(Request::uri(), Request::method()); // direct the request to the specified route
+
+if(!function_exists("view")) {
+    function view($view, $data = []){
+        $viewFileExt = '.view.php';
+        $view .= $viewFileExt;
+        extract($data);
+
+        return require "app/views/master{$viewFileExt}";
+    }
+}
+
