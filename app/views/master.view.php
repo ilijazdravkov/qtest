246abc513bc596bcfe70d2bb1e06b75148ac70bd
ILIJA ZDRAVKOV <ilija.zdravkov@polipanelgroup.com>
2020-04-13 14:37:19 +0200

    add css style, bootstrap css and js, proper.js

--- app/views/master.view.php
+++ app/views/master.view.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
 
